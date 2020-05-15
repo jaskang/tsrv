5bf1b46359075073242edd4dcc3fafef2cda9127
jaskang <kangchaomin@dayuwuxian.com>
2020-05-16 01:33:16 +0800

    test: default export vue

---
+++ test/e2e/fixtures/build-vue/src/components/button/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>button</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'button'
+})
+</script>
+
+<style lang="less">
+.test {
+}
+</style>
--- test/e2e/fixtures/build-vue/src/index.ts
+++ test/e2e/fixtures/build-vue/src/index.ts
@@ -1,4 +1,6 @@
 import scoped from './scoped.vue'
 import module from './module.vue'
 
+export { default as button } from './components/index.vue'
+
 export { scoped, module }
