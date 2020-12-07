82915b30bc69fa5214132314afe813a18dc8b7a8
jaskang <kangchaomin@dayuwuxian.com>
2020-12-08 00:37:12 +0800

    chore: remove test

--- test/e2e/fixtures/build-vue/src/index.ts
+++
@@ -1,7 +0,0 @@
-import scoped from './scoped.vue'
-import module from './module.vue'
-import css from './css.vue'
-
-export { default as button } from './components/button/index.vue'
-export { default as TestTeleport } from './components/TestTeleport'
-export { scoped, module, css }
--- test/e2e/fixtures/build-vue/types/shims-vue.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.vue' {
-  import { ComponentOptions } from 'vue'
-  const comp: ComponentOptions
-  export default comp
-}
-declare module '*.module.less' {
-  const classes: { readonly [key: string]: string }
-  export default classes
-}
