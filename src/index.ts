e42d0d4b494eded78ac86e1d5292322098c1e63e
jaskang <kangchaomin@dayuwuxian.com>
2020-12-11 11:44:12 +0800

    chore: export TsrvUserConfig

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { execRollup, watchRollup } from './modules/rollup'
 export { execJest } from './modules/jest'
+export { TsrvUserConfig } from './config'
 export { default as babelPresetElenext } from './modules/babelPresetElenext'
