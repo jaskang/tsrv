b767515dc02c6a92d7073618aa76cd46f9961a3f
jaskang <kangchaomin@dayuwuxian.com>
2020-12-11 22:09:03 +0800

    fix: dts remove style import

--- src/modules/rollup/createDts.ts
+++ src/modules/rollup/createDts.ts
@@ -5,6 +5,7 @@ import dts from 'rollup-plugin-dts'
 export async function createDts(input: string, output: string) {
   const bundle = await rollup({
     input: input,
+    external: [/\.scss$/, /\.less$/, /\.css$/],
     plugins: [dts()]
   })
   await bundle.write({ file: output, format: 'es' })
